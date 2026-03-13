cdd6d45a957ab9e32de55e42503c5f68c24a7a32
Enator18 <62544405+Enator18@users.noreply.github.com>
2026-03-13 14:44:06 -0400

    switched from adaptive vsync to regular

--- main.cpp
+++ main.cpp
@@ -306,7 +306,7 @@ int main()
     }
 
     SDL_SetRenderLogicalPresentation(renderer, MAP_WIDTH, MAP_WIDTH + 2, SDL_LOGICAL_PRESENTATION_LETTERBOX);
-    SDL_SetRenderVSync(renderer, SDL_RENDERER_VSYNC_ADAPTIVE);
+    SDL_SetRenderVSync(renderer, 1);
 
     groundTile = LoadTexture("ground_tile");
     treeTile = LoadTexture("tree_base");
