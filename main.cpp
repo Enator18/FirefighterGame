b47875f8121d534232ce3427e992f80cfb2bf486
Enator18 <62544405+Enator18@users.noreply.github.com>
2026-04-22 00:49:15 -0400

    added MapState and ActionState (broken)

--- main.cpp
+++ main.cpp
@@ -42,13 +42,18 @@ SDL_Texture* fontAtlas;
 i32 windowWidth;
 i32 windowHeight;
 
-ivec2 newPlayerPos;
-f32 moveTimer;
+enum ActionState
+{
+    MOVING,
+    WATER,
+    IDLE
+};
 
-ivec2 waterTarget;
-f32 waterTime;
-f32 waterAnimTimer;
-bool water;
+ActionState currentState;
+
+ivec2 animTarget;
+f32 animTimer;
+f32 animTime;
 
 u32 startingTrees;
 u32 maxLostTrees;
@@ -63,21 +68,109 @@ struct MapCell
     bool hasTank = false;
 };
 
+struct MapState
+{
+    std::array<MapCell, MAP_WIDTH * MAP_WIDTH> tiles{};
+    u32 lostTrees;
+};
+
 struct GameState
 {
-    std::array<MapCell, MAP_WIDTH * MAP_WIDTH> map{};
+    MapState map;
     ivec2 playerPos;
-    u32 lostTrees;
     bool bigWater = false;
 };
 
 GameState state;
+MapState nextMap;
 
 std::vector<GameState> previousStates;
 
-inline MapCell& CellAt(ivec2 pos)
+inline MapCell& CellAt(MapState& map, ivec2 pos)
 {
-    return state.map[pos.x + pos.y * MAP_WIDTH];
+    return map.tiles[pos.x + pos.y * MAP_WIDTH];
+}
+
+inline bool PosInBounds(i32 pos)
+{
+    return pos >= 0 && pos < MAP_WIDTH;
+}
+
+inline bool PosInBounds(ivec2 pos)
+{
+    return PosInBounds(pos.x) && PosInBounds(pos.y);
+}
+
+void IgniteNeighbors(i32 row, i32 col)
+{
+    for (i32 rowOff = -1; rowOff <= 1; rowOff++)
+    {
+        for (i32 colOff = -1; colOff <= 1; colOff++)
+        {
+            i32 neighborRow = row + rowOff;
+            i32 neighborCol = col + colOff;
+            if (PosInBounds(neighborRow) && PosInBounds(neighborCol))
+            {
+                MapCell& cell = CellAt(nextMap, {neighborCol, neighborRow});
+                if (cell.hasTree && cell.fireCount < 5)
+                {
+                    cell.fireCount++;
+                }
+            }
+        }
+    }
+}
+
+void CheckWin()
+{
+    u32 totalFire = 0;
+    for (u32 row = 0; row < MAP_WIDTH; row++)
+    {
+        for (u32 col = 0; col < MAP_WIDTH; col++)
+        {
+            if (CellAt(state.map, {col, row}).fireCount > 0)
+            {
+                totalFire++;
+            }
+        }
+    }
+
+    if (totalFire == 0)
+    {
+        gameWon = true;
+    }
+}
+
+void AdvanceTime()
+{
+    previousStates.push_back(state);
+    state.map = nextMap;
+    for (u32 row = 0; row < MAP_WIDTH; row++)
+    {
+        for (u32 col = 0; col < MAP_WIDTH; col++)
+        {
+            MapCell& cell = CellAt(nextMap, {col, row});
+            if (cell.fireCount > 0)
+            {
+                cell.fireCount++;
+            }
+        }
+    }
+    for (u32 row = 0; row < MAP_WIDTH; row++)
+    {
+        for (u32 col = 0; col < MAP_WIDTH; col++)
+        {
+            MapCell& cell = CellAt(nextMap, {col, row});
+            if (cell.fireCount > 5)
+            {
+                cell.fireCount = 0;
+                cell.hasTree = false;
+                IgniteNeighbors(row, col);
+                nextMap.lostTrees++;
+            }
+        }
+    }
+    CheckWin();
 }
 
 bool LoadLevel(u32 levelNum)
@@ -91,10 +184,7 @@ bool LoadLevel(u32 levelNum)
 
     std::ifstream mapFile{filename};
 
-    previousStates.clear();
-
-    moveTimer = 0;
-    water = false;
+    currentState = IDLE;
     gameWon = false;
 
     std::string line;
@@ -103,7 +193,7 @@ bool LoadLevel(u32 levelNum)
         return false;
     }
     maxLostTrees = std::stoi(line);
-    state.lostTrees = 0;
+    state.map.lostTrees = 0;
     state.bigWater = false;
     startingTrees = 0;
 
@@ -122,56 +212,49 @@ bool LoadLevel(u32 levelNum)
             switch (line[col])
             {
             case 'T':
-                CellAt({col, row}) = {0, true};
+                CellAt(state.map, {col, row}) = {0, true};
                 startingTrees++;
                 break;
             case 'P':
                 state.playerPos = {col, row};
-                newPlayerPos = state.playerPos;
-                CellAt({col, row}) = {};
+                CellAt(state.map, {col, row}) = {};
                 break;
             case 'W':
-                CellAt({col, row}) = {0, false, true};
+                CellAt(state.map, {col, row}) = {0, false, true};
                 break;
             case '1':
-                CellAt({col, row}) = {1, true};
+                CellAt(state.map, {col, row}) = {1, true};
                 startingTrees++;
                 break;
             case '2':
-                CellAt({col, row}) = {2, true};
+                CellAt(state.map, {col, row}) = {2, true};
                 startingTrees++;
                 break;
             case '3':
-                CellAt({col, row}) = {3, true};
+                CellAt(state.map, {col, row}) = {3, true};
                 startingTrees++;
                 break;
             case '4':
-                CellAt({col, row}) = {4, true};
+                CellAt(state.map, {col, row}) = {4, true};
                 startingTrees++;
                 break;
             case '5':
-                CellAt({col, row}) = {5, true};
+                CellAt(state.map, {col, row}) = {5, true};
                 startingTrees++;
                 break;
             default:
-                CellAt({col, row}) = {};
+                CellAt(state.map, {col, row}) = {};
                 break;
             }
         }
     }
 
+    nextMap = state.map;
+    AdvanceTime();
+    previousStates.clear();
+
     currentLevel = levelNum;
     return true;
-}
-
-inline bool PosInBounds(i32 pos)
-{
-    return pos >= 0 && pos < MAP_WIDTH;
-}
-
-inline bool PosInBounds(ivec2 pos)
-{
-    return PosInBounds(pos.x) && PosInBounds(pos.y);
 }
 
 SDL_Texture* LoadTexture(std::string filename)
@@ -187,90 +270,19 @@ SDL_Texture* LoadTexture(std::string filename)
     return texture;
 }
 
-void IgniteNeighbors(i32 row, i32 col)
-{
-    for (i32 rowOff = -1; rowOff <= 1; rowOff++)
-    {
-        for (i32 colOff = -1; colOff <= 1; colOff++)
-        {
-            i32 neighborRow = row + rowOff;
-            i32 neighborCol = col + colOff;
-            if (PosInBounds(neighborRow) && PosInBounds(neighborCol))
-            {
-                MapCell& cell = CellAt({neighborCol, neighborRow});
-                if (cell.hasTree && cell.fireCount < 5)
-                {
-                    cell.fireCount++;
-                }
-            }
-        }
-    }
-}
-
-void CheckWin()
-{
-    u32 totalFire = 0;
-    for (u32 row = 0; row < MAP_WIDTH; row++)
-    {
-        for (u32 col = 0; col < MAP_WIDTH; col++)
-        {
-            if (CellAt({col, row}).fireCount > 0)
-            {
-                totalFire++;
-            }
-        }
-    }
-
-    if (totalFire == 0)
-    {
-        gameWon = true;
-    }
-}
-
-void AdvanceTime()
-{
-    previousStates.push_back(state);
-    for (u32 row = 0; row < MAP_WIDTH; row++)
-    {
-        for (u32 col = 0; col < MAP_WIDTH; col++)
-        {
-            MapCell& cell = CellAt({col, row});
-            if (cell.fireCount > 0)
-            {
-                cell.fireCount++;
-            }
-        }
-    }
-    for (u32 row = 0; row < MAP_WIDTH; row++)
-    {
-        for (u32 col = 0; col < MAP_WIDTH; col++)
-        {
-            MapCell& cell = CellAt({col, row});
-            if (cell.fireCount > 5)
-            {
-                cell.fireCount = 0;
-                cell.hasTree = false;
-                IgniteNeighbors(row, col);
-                state.lostTrees++;
-            }
-        }
-    }
-    CheckWin();
-}
-
 void ShootWater(ivec2 direction)
 {
     AdvanceTime();
-    water = true;
-    waterAnimTimer = 1.0;
-    waterTime = 0;
+    currentState = WATER;
+    animTimer = 1.0;
+    animTime = 0;
     ivec2 pos = state.playerPos;
-    while (PosInBounds(pos) && !CellAt(pos).hasTree)
+    while (PosInBounds(pos) && !CellAt(state.map, pos).hasTree)
     {
         pos += direction;
-        waterTime += WATER_MOVE_TIME;
+        animTime += WATER_MOVE_TIME;
     }
-    waterTarget = pos;
+    animTarget = pos;
 }
 
 void DrawText(std::string& text, vec2 pos)
@@ -301,6 +313,141 @@ void DrawText(std::string& text, vec2 pos)
             currentPos.x += width / 16.0f;
         }
     }
+}
+
+bool HandleKey(SDL_Event event)
+{
+    if (event.key.key == SDLK_ESCAPE)
+    {
+        return false;
+    }
+    if (currentState == IDLE)
+    {
+        ivec2 nextPos = state.playerPos;
+        if (event.key.key == SDLK_W && nextPos.y > 0)
+        {
+            nextPos.y--;
+        }
+        if (event.key.key == SDLK_S && nextPos.y < MAP_WIDTH - 1)
+        {
+            nextPos.y++;
+        }
+        if (event.key.key == SDLK_A && nextPos.x > 0)
+        {
+            nextPos.x--;
+        }
+        if (event.key.key == SDLK_D && nextPos.x < MAP_WIDTH - 1)
+        {
+            nextPos.x++;
+        }
+        if (nextPos != state.playerPos)
+        {
+            if (!CellAt(state.map, nextPos).hasTree)
+            {
+                animTimer = 1.0;
+                animTime = MOVE_TIME;
+                animTarget = nextPos;
+                currentState = MOVING;
+                MapCell& newCell = CellAt(state.map, nextPos);
+                if (newCell.hasTank)
+                {
+                    newCell.hasTank = false;
+                    state.bigWater = true;
+                }
+            }
+        }
+        switch (event.key.key)
+        {
+            case SDLK_UP:
+                ShootWater({0, -1});
+                break;
+            case SDLK_DOWN:
+                ShootWater({0, 1});
+                break;
+            case SDLK_LEFT:
+                ShootWater({-1, 0});
+                break;
+            case SDLK_RIGHT:
+                ShootWater({1, 0});
+                break;
+            case SDLK_SPACE:
+                AdvanceTime();
+                break;
+            case SDLK_R:
+                LoadLevel(currentLevel);
+                break;
+            case SDLK_Z:
+                if (!previousStates.empty())
+                {
+                    nextMap = state.map;
+                    state = previousStates.back();
+                    previousStates.pop_back();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    return true;
+}
+
+void DrawFrame()
+{
+    SDL_SetRenderDrawColor(renderer, 0xcf, 0x19, 0x19, 0xff);
+    SDL_RenderClear(renderer);
+
+    SDL_FRect background = {0, 0, 9, 9};
+    SDL_RenderTextureTiled(renderer, groundTile, nullptr, 1.0 / groundTile->w, &background);
+
+    if (currentState == WATER)
+    {
+        vec2 waterPosInterp = static_cast<vec2>(animTarget) +
+                        (static_cast<vec2>(state.playerPos) - static_cast<vec2>(animTarget)) * animTimer;
+        SDL_FRect waterRect = {waterPosInterp.x, waterPosInterp.y, 1, 1};
+        if (state.bigWater)
+        {
+            SDL_RenderTexture(renderer, bigWaterSprite, nullptr, &waterRect);
+        }
+        else
+        {
+            SDL_RenderTexture(renderer, waterSprite, nullptr, &waterRect);
+        }
+    }
+
+    for (f32 row = 0; row < MAP_WIDTH; row++)
+    {
+        for (f32 col = 0; col < MAP_WIDTH; col++)
+        {
+            SDL_FRect tileRect = {col, row, 1, 1};
+
+            MapCell& cell = CellAt(state.map, {col, row});
+            if (cell.hasTree)
+            {
+                SDL_RenderTexture(renderer, treeTile, nullptr, &tileRect);
+
+                if (cell.fireCount > 0)
+                {
+                    SDL_RenderTexture(renderer, flameTiles[cell.fireCount - 1], nullptr, &tileRect);
+                }
+            }
+            if (cell.hasTank)
+            {
+                SDL_RenderTexture(renderer, waterTankTile, nullptr, &tileRect);
+            }
+        }
+    }
+
+    vec2 playerPosInterp = static_cast<vec2>(animTarget) +
+        (static_cast<vec2>(state.playerPos) - static_cast<vec2>(animTarget)) * animTimer;
+    vec2 playerDrawPos = currentState == MOVING ? playerPosInterp : vec2(state.playerPos);
+    SDL_FRect playerRect = {playerDrawPos.x, playerDrawPos.y, 1, 1};
+    SDL_RenderTexture(renderer, playerSprite, nullptr, &playerRect);
+
+    std::string status = std::format("Level {}\nTrees Alive: {}\nMinimum Trees: {}",
+        currentLevel, startingTrees - state.map.lostTrees, startingTrees - maxLostTrees);
+    DrawText(status, {0.25, MAP_WIDTH + 0.125});
+
+    SDL_RenderPresent(renderer);
 }
 
 int main()
@@ -363,78 +510,10 @@ int main()
             {
             case SDL_EVENT_QUIT:
                 return 0;
-
             case SDL_EVENT_KEY_DOWN:
-                if (event.key.key == SDLK_ESCAPE)
+                if (!HandleKey(event))
                 {
                     return 0;
-                }
-                if (moveTimer <= 0 && !water)
-                {
-                    state.playerPos = newPlayerPos;
-                    if (event.key.key == SDLK_W && newPlayerPos.y > 0)
-                    {
-                        newPlayerPos.y--;
-                    }
-                    if (event.key.key == SDLK_S && newPlayerPos.y < MAP_WIDTH - 1)
-                    {
-                        newPlayerPos.y++;
-                    }
-                    if (event.key.key == SDLK_A && newPlayerPos.x > 0)
-                    {
-                        newPlayerPos.x--;
-                    }
-                    if (event.key.key == SDLK_D && newPlayerPos.x < MAP_WIDTH - 1)
-                    {
-                        newPlayerPos.x++;
-                    }
-                    if (newPlayerPos != state.playerPos)
-                    {
-                        if (CellAt(newPlayerPos).hasTree)
-                        {
-                            newPlayerPos = state.playerPos;
-                        }
-                        else
-                        {
-                            moveTimer = 1.0;
-                            MapCell& newCell = CellAt(newPlayerPos);
-                            if (newCell.hasTank)
-                            {
-                                newCell.hasTank = false;
-                                state.bigWater = true;
-                            }
-                            AdvanceTime();
-                        }
-                    }
-                    switch (event.key.key)
-                    {
-                        case SDLK_UP:
-                            ShootWater({0, -1});
-                            break;
-                        case SDLK_DOWN:
-                            ShootWater({0, 1});
-                            break;
-                        case SDLK_LEFT:
-                            ShootWater({-1, 0});
-                            break;
-                        case SDLK_RIGHT:
-                            ShootWater({1, 0});
-                            break;
-                        case SDLK_SPACE:
-                            AdvanceTime();
-                            break;
-                        case SDLK_R:
-                            LoadLevel(currentLevel);
-                            break;
-                        case SDLK_Z:
-                            if (!previousStates.empty())
-                            {
-                                state = previousStates.back();
-                                previousStates.pop_back();
-                                newPlayerPos = state.playerPos;
-                            }
-                            break;
-                    }
                 }
                 break;
             default:
@@ -442,38 +521,25 @@ int main()
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 0xcf, 0x19, 0x19, 0xff);
-        SDL_RenderClear(renderer);
-
-        SDL_FRect background = {0, 0, 9, 9};
-        SDL_RenderTextureTiled(renderer, groundTile, nullptr, 1.0 / groundTile->w, &background);
-
-        if (moveTimer > 0)
+        if (animTimer >= 0)
         {
-            moveTimer -= delta / MOVE_TIME;
+            animTimer -= delta / animTime;
         }
-        if (moveTimer < 0)
+        if (animTimer < 0)
         {
-            moveTimer = 0;
-        }
-
-        if (water)
-        {
-            waterAnimTimer -= delta / waterTime;
-
-            if (waterAnimTimer < 0)
+            animTimer = 0;
+            if (currentState == WATER)
             {
-                water = false;
                 if (state.bigWater)
                 {
                     for (i32 x = -1; x < 2; x++)
                     {
                         for (i32 y = -1; y < 2; y++)
                         {
-                            ivec2 treePos = waterTarget + ivec2{x, y};
+                            ivec2 treePos = animTarget + ivec2{x, y};
                             if (PosInBounds(treePos))
                             {
-                                CellAt(treePos).fireCount = 0;
+                                CellAt(state.map, treePos).fireCount = 0;
                             }
                             CheckWin();
                         }
@@ -481,63 +547,24 @@ int main()
                 }
                 else
                 {
-                    if (PosInBounds(waterTarget))
+                    if (PosInBounds(animTarget))
                     {
-                        CellAt(waterTarget).fireCount = 0;
+                        CellAt(state.map, animTarget).fireCount = 0;
                         CheckWin();
                     }
                 }
                 state.bigWater = false;
             }
-
-            vec2 waterPosInterp = static_cast<vec2>(waterTarget) +
-                            (static_cast<vec2>(state.playerPos) - static_cast<vec2>(waterTarget)) * waterAnimTimer;
-            SDL_FRect waterRect = {waterPosInterp.x, waterPosInterp.y, 1, 1};
-            if (state.bigWater)
+            else if (currentState == MOVING)
             {
-                SDL_RenderTexture(renderer, bigWaterSprite, nullptr, &waterRect);
+                state.playerPos = animTarget;
             }
-            else
-            {
-                SDL_RenderTexture(renderer, waterSprite, nullptr, &waterRect);
-            }
+            currentState = IDLE;
         }
 
-        for (f32 row = 0; row < MAP_WIDTH; row++)
-        {
-            for (f32 col = 0; col < MAP_WIDTH; col++)
-            {
-                SDL_FRect tileRect = {col, row, 1, 1};
+        DrawFrame();
 
-                MapCell& cell = CellAt({col, row});
-                if (cell.hasTree)
-                {
-                    SDL_RenderTexture(renderer, treeTile, nullptr, &tileRect);
-
-                    if (cell.fireCount > 0)
-                    {
-                        SDL_RenderTexture(renderer, flameTiles[cell.fireCount - 1], nullptr, &tileRect);
-                    }
-                }
-                if (cell.hasTank)
-                {
-                    SDL_RenderTexture(renderer, waterTankTile, nullptr, &tileRect);
-                }
-            }
-        }
-
-        vec2 playerPosInterp = static_cast<vec2>(newPlayerPos) +
-            (static_cast<vec2>(state.playerPos) - static_cast<vec2>(newPlayerPos)) * moveTimer;
-        SDL_FRect playerRect = {playerPosInterp.x, playerPosInterp.y, 1, 1};
-        SDL_RenderTexture(renderer, playerSprite, nullptr, &playerRect);
-
-        std::string status = std::format("Level {}\nTrees Alive: {}\nMinimum Trees: {}",
-            currentLevel, startingTrees - state.lostTrees, startingTrees - maxLostTrees);
-        DrawText(status, {0.25, MAP_WIDTH + 0.125});
-
-        SDL_RenderPresent(renderer);
-
-        if (state.lostTrees > maxLostTrees)
+        if (state.map.lostTrees > maxLostTrees)
         {
             SDL_ShowSimpleMessageBox(
                 SDL_MESSAGEBOX_INFORMATION,
